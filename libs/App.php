6c74027151cb955cad9de9870252a17c83b339f6
lenochware <lenochware@gmail.com>
2025-03-03 10:33:03 +0100

    optimalization

--- libs/App.php
+++ libs/App.php
@@ -4,6 +4,10 @@ class App extends pclib\App {
 
 	function initDatabase()
 	{
+		if (!$this->db->info) {
+			$this->error('Není nastavené připojení k databázi.');
+		}
+
 		if (($this->db->info['driver'] ?? '') == 'pgsql') {
 		  $this->db->drv->ucase = 1;
 		  $this->db->drv->noquote = 1;
@@ -57,10 +61,12 @@ class App extends pclib\App {
 	{
 	  try {
 	    $table = (get_class($this->db->drv) == 'pgsql')? 'auth_users':'AUTH_USERS';
-	    return $this->db->columns($table)? true:false;
+	    $this->db->select($table);
 	  } catch(Exception $e) {
-	    $this->error('Nepodařilo se připojit k databázi. Chyba: %s',null, $e->getMessage());
+	    return false;
 	  }
+
+	  return true;
 	}
 
 }
